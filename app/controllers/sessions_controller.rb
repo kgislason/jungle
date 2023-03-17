f119c3e9bfe79a0a04058be97543843b768591bb
Kristy Gislason <kristygislason@gmail.com>
2023-03-17 13:24:44 -0700

    Update controller to match existing style of other previous controllers for user and session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to [:root], notice: 'Successful login!'
     else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to [:login]
   end
 
 end
