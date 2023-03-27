b2552973b4677dd02b48dea7716d988b189c7849
Kristy Gislason <kristygislason@gmail.com>
2023-03-27 13:11:04 -0700

    Refactor tests to use .include? so it searches errors.full_messages array rather than using array[0]

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -31,19 +31,19 @@ RSpec.describe Product, type: :model do
           category: @category
         )
         expect(@product.valid?).to be false
-        expect(@product.errors.full_messages[0]).to eq("Name can't be blank")
+        expect(@product.errors.full_messages.include? "Name can't be blank").to be true
       end
 
       it 'should not save if the Price is not present' do
         @category = Category.new(name: "Test Category")
         @product = Product.new(
           name: "Test Product",
-          # price: nil,
+          price_cents: nil,
           quantity: 100,
           category: @category
         )
         expect(@product.valid?).to be false
-        expect(@product.errors.full_messages[0]).to eq("Price cents is not a number")
+        expect(@product.errors.full_messages.include? "Price cents is not a number").to be true
       end
 
       it 'should not save if the quantity is not present' do
@@ -55,7 +55,7 @@ RSpec.describe Product, type: :model do
           category: @category
         )
         expect(@product.valid?).to be false
-        expect(@product.errors.full_messages[0]).to eq("Quantity can't be blank")
+        expect(@product.errors.full_messages.include? "Quantity can't be blank").to be true
       end
 
       it 'should not save if the category is not present' do
@@ -68,7 +68,7 @@ RSpec.describe Product, type: :model do
         )
         puts @product.errors.full_messages[0]
         expect(@product.valid?).to be false
-        expect(@product.errors.full_messages[0]).to eq("Category must exist")
+        expect(@product.errors.full_messages.include? "Category must exist").to be true
       end
     end
   end
