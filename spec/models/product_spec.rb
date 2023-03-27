9269301886b6b7f796d44acc967a28cd51199d1e
Kristy Gislason <kristygislason@gmail.com>
2023-03-27 12:19:35 -0700

    Add model spec for Product with tests to validate 4 required fields name, price, quantity and category

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe '#id' do
+    it 'should not exist for new records' do
+      @product = Product.new
+      expect(@product.id).to be_nil
+    end
+
+    describe 'Validations' do
+      it 'should save product if all equired fields are present' do
+        @category = Category.new(name: "Test Category")
+        @product = Product.new(
+          name: "Test Product",
+          price: 123,
+          quantity: 100,
+          category: @category
+        )
+        @product.save!
+        expect(@product.valid?).to be true
+        expect(@product.id).to be_present
+      end
+
+      it 'should not save if the name is not present' do
+        @category = Category.new(name: "Test Category")
+        @product = Product.new(
+          name: nil,
+          price: 123,
+          quantity: 100,
+          category: @category
+        )
+        expect(@product.valid?).to be false
+        expect(@product.errors.full_messages[0]).to eq("Name can't be blank")
+      end
+
+      it 'should not save if the Price is not present' do
+        @category = Category.new(name: "Test Category")
+        @product = Product.new(
+          name: "Test Product",
+          # price: nil,
+          quantity: 100,
+          category: @category
+        )
+        expect(@product.valid?).to be false
+        expect(@product.errors.full_messages[0]).to eq("Price cents is not a number")
+      end
+
+      it 'should not save if the quantity is not present' do
+        @category = Category.new(name: "Test Category")
+        @product = Product.new(
+          name: "Test Product",
+          price: 123,
+          quantity: nil,
+          category: @category
+        )
+        expect(@product.valid?).to be false
+        expect(@product.errors.full_messages[0]).to eq("Quantity can't be blank")
+      end
+
+      it 'should not save if the category is not present' do
+        @category = Category.new(name: "Test Category")
+        @product = Product.new(
+          name: "Test Product",
+          price: 123,
+          quantity: 100,
+          category: nil
+        )
+        puts @product.errors.full_messages[0]
+        expect(@product.valid?).to be false
+        expect(@product.errors.full_messages[0]).to eq("Category must exist")
+      end
+    end
+  end
+
+end
