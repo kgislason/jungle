dc626cdfb0603ccf36205afccf6b000c356664a8
Kristy Gislason <kristygislason@gmail.com>
2023-03-28 21:54:56 -0700

    Add test to add product to cart and change cart quantity by 1

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,13 +1,14 @@
-describe('Visit the product details page', () => {
+describe('Visit the add to cart page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
   it("The user can click on the product details page for a product", () => {
     cy.get(".products article").should("have.length", 2);
-
     cy.get(".products article a").first().click();
-    cy.get("article img").should("have.class", "main-img")
+    cy.get("article img").should("have.class", "main-img");
+    cy.get("article button.btn").click();
+    cy.get('[data-testid="cart-quantity"]').should("have.text", "(1)")
   });
 
 })
